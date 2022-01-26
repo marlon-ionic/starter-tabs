32101f1ad2592484e2dd533dbb5815b5008b2c43
Marlon Harrison <marlon@ionic.io>
2022-01-25 18:46:54 -0600

    add android, ios, and iframe example

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, NgZone, ViewChild } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-tab1',
@@ -6,7 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  isScrollingAgreement = true;
+  safeFrameResourceURL: SafeResourceUrl;
+  constructor(private sanitizer: DomSanitizer, private zone: NgZone) {
+    //Sets the src for the iframe and passes the origin. This just makes it easier to handle different environments for the same iframe page
+    const url = `https://marlonharrison.com/static/demos/eula.html?o=${location.origin}`;
+    this.safeFrameResourceURL = this.sanitizer.bypassSecurityTrustResourceUrl(url);
+  }
+  frameLoaded(e: Event) {
+    window.addEventListener('message', (evt: MessageEvent) => {
+      console.log('message!', evt);
+      this.zone.run(() => this.isScrollingAgreement = !evt.data.hasScrolledToEnd);
+    });
+  }
 
-  constructor() {}
-
+  agreeClick(){
+    console.log('agree click!');
+  }
 }
