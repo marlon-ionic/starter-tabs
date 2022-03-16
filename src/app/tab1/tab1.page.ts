e249fd7618350cc2c30e960fd9a38f13ee484d64
Marlon Harrison <marlon@ionic.io>
2022-03-16 13:48:01 -0500

    add toggle button

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -9,19 +9,28 @@ import { Platform } from '@ionic/angular';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
-
+  isUnlocked = false;
   constructor(private readonly screenOrientation: ScreenOrientation, private readonly platform: Platform) {
     this.init();
   }
 
+  async toggle() {
+    if(this.platform.is('hybrid')) {
+      if(this.isUnlocked) {
+        await this.screenOrientation.lock(this.screenOrientation.ORIENTATIONS.PORTRAIT);
+        this.isUnlocked = false;
+      } else {
+        await this.screenOrientation.unlock();
+        this.isUnlocked = true;
+      }
+    }
+  }
 
   private async init(): Promise <void> {
     if(this.platform.is('hybrid')) {
       await this.platform.ready();
-      await this.screenOrientation.lock(this.screenOrientation.ORIENTATIONS.LANDSCAPE);
+      await this.screenOrientation.lock(this.screenOrientation.ORIENTATIONS.PORTRAIT);
     }
   }
-
-
 
 }
