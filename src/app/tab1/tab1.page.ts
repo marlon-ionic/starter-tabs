e1912c02bd0461262c20b6e068b5f5fb45d3b8f6
Marlon Harrison <marlon@ionic.io>
2022-05-26 15:27:41 -0500

    tab1

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { WithIonContentComponent } from '../modals/with-ion-content/with-ion-content.component';
+import { WithoutIonContentComponent } from '../modals/without-ion-content/without-ion-content.component';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +10,17 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(private modalController: ModalController) {}
+
+  async present(withIonContent = false) {
+    const modal = await this.modalController.create({
+      component: withIonContent ? WithIonContentComponent : WithoutIonContentComponent,
+      cssClass: 'mobile-transfer-confirmation'
+    });
+
+    await modal.present();
+  }
+
+
 
 }
