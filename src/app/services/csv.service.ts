6f35fd9dd3491bd7d59b46ae3d58720f4132a9a6
Marlon Harrison <marlon@ionic.io>
2022-05-26 15:29:27 -0500

    add csvService

---
+++ src/app/services/csv.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CsvService {
+
+  downloadFile(data, filename='data') {
+    const csvData = this.convertToCSV(data, ['name','age', 'average', 'approved', 'description']);
+    console.log(csvData);
+    const blob = new Blob(['\ufeff' + csvData], { type: 'text/csv;charset=utf-8;' });
+    const dwldLink = document.createElement('a');
+    const url = URL.createObjectURL(blob);
+    const isSafariBrowser = navigator.userAgent.indexOf('Safari') != -1 && navigator.userAgent.indexOf('Chrome') == -1;
+    if (isSafariBrowser) {  //if Safari open in new window to save file with random filename.
+        dwldLink.setAttribute('target', '_blank');
+    }
+    dwldLink.setAttribute('href', url);
+    dwldLink.setAttribute('download', filename + '.csv');
+    dwldLink.style.visibility = 'hidden';
+    document.body.appendChild(dwldLink);
+    dwldLink.click();
+    console.log('isSafariBrowser', isSafariBrowser, `${filename}.csv`);
+    // document.body.removeChild(dwldLink);
+}
+
+convertToCSV(objArray, headerList) {
+     const array = typeof objArray != 'object' ? JSON.parse(objArray) : objArray;
+     let str = '';
+     let row = 'S.No,';
+
+     for (const index in headerList) {
+         row += headerList[index] + ',';
+     }
+     row = row.slice(0, -1);
+     str += row + '\r\n';
+     for (let i = 0; i < array.length; i++) {
+         let line = (i+1)+'';
+         for (const index in headerList) {
+            const head = headerList[index];
+
+             line += ',' + array[i][head];
+         }
+         str += line + '\r\n';
+     }
+     return str;
+ }
+}
